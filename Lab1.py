8a9b36595caa637684a39e90c22568b8444e62f9
Isaac Doulfikar <isaacddoulf@gmail.comgit config --global user.name>
2026-09-13 17:15:28 -0700

    Lab 1

--- Lab1.py
+++ Lab1.py
@@ -3,10 +3,10 @@ print("Isaac Doulfikar")
 
 # The area of a rectangle is length * width.
 # What is the area of a rectangle with a length of 10 and width of 7?
-print("70 square units")
+print(70)
 
 # Fix the line below. Interpreters scan your entire file ahead-of-time to check for errors, unlike compilers.
-print("false")
+print(False)
 
 # Add one more color to the end of the list, after blue. (Don't forget the comma , and quotes "")
 print(["red", "orange", "blue", "green"])
