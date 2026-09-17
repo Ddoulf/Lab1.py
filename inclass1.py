ec6b47dbfa03d12a3285602aa8fb1a6b6a41a9c4
Isaac Doulfikar <isaacddoulf@gmail.comgit config --global user.name>
2026-09-17 16:05:53 -0700

    submit new inclass

---
+++ inclass1.py
@@ -0,0 +1,26 @@
+# Create a variable named x and store an integer (whole number) inside it
+x = 10
+print(x)
+# Create a variable named y and store a string (any characters between single or double quotes) inside it
+y = ("string")
+print(y)
+# Create a variable named z and store a float in it
+number = 7
+z = float(number)
+print(z)
+
+# Change the value stored in the x variable to a new string
+x = "abcit"
+print(x)
+# Change the value stored in the y variable to a new boolean
+y = True
+print(y)
+# Change the value store in the z variable to a different float
+z = 8.5
+print(z)
+
+# Create two variables, one containing the string CIT, and another containing the string 1515
+c = "CIT"
+n = "1515"
+# Using the two variables and a hard-coded letter, print the word ACIT1515 to the terminal
+print("A" + c + n)
